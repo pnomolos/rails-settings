5053ef6d605cb9f5c45c30d0bf75ffb4baefbaf5
Philip <pnomolos@gmail.com>
2010-09-19 01:33:08 -0700

    OK.  I really think this should work now.

--- lib/rails-settings/railtie.rb
+++ lib/rails-settings/railtie.rb
@@ -1,7 +1,8 @@
+require 'rails'
+require 'rails-settings'
+
 module RailsSettings
   class Railtie < Rails::Railtie
-    railtie_name :rails_settings
-    
     initializer "rails_settings.init_rails" do |app|
       RailsSetttings.run
     end
