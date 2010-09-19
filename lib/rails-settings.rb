f0c8348eb957197e78d04438a0a79d751bc57c68
Philip <pnomolos@gmail.com>
2010-09-19 01:18:16 -0700

    I *think* this will work?

--- lib/rails-settings.rb
+++ lib/rails-settings.rb
@@ -1,81 +1,14 @@
 require 'rails-settings/railtie'
 
+load File.join(File.dirname(__FILE__), "rails-settings", "initializer.rb")
 
-
-
-
+RS = RailsSettings
+RS.register_adapter :datamapper, "#{adapter_path}/datamapper" if defined?(DataMapper)
 module RailsSettings
-  load File.join(File.dirname(__FILE__), "rails-settings", "initializer.rb")
-  
-  require 'lib/rails-settings/scoped'
-end
-
-if defined?(Merb::Plugins)
-
-  require "yaml"
-
-  $:.unshift File.dirname(__FILE__)
-
-  load File.join(File.dirname(__FILE__), "merb-settings", "initializer.rb")
-
-  load_dependency 'merb-slices', nil
-  Merb::Plugins.add_rakefiles "merb-settings/merbtasks", "merb-settings/slicetasks"
-
-  # Register the Slice for the current host application
-  Merb::Slices::register(__FILE__)
-  
-  # Slice configuration - set this in a before_app_loads callback.
-  #Merb::Slices::config[:merb_settings][:foo] ||= :bar
-
-  adapter_path = File.join( File.dirname(__FILE__), "merb-settings", "adapters")
-
-  MS = MerbSettings
-  MS.register_adapter :datamapper, "#{adapter_path}/datamapper"
-  #MS.register_adapter :activerecord, "#{adapter_path}/activerecord"
-  
-  # All Slice code is expected to be namespaced inside a module
-  module MerbSettings
-    
-    # Slice metadata
-    self.description = "MerbSettings"
-    self.version = "0.0.1"
-    self.author = "Felix McCoey"
-    
-    # Stub classes loaded hook - runs before LoadClasses BootLoader
-    # right after a slice's classes have been loaded internally.
-    def self.loaded
-      MS.load_adapter!
-    end
-    
-    # Initialization hook - runs before AfterAppLoads BootLoader
-    def self.init
-    end
-    
-    # Activation hook - runs after AfterAppLoads BootLoader
-    def self.activate
-    end
-    
-    # Deactivation hook - triggered by Merb::Slices.deactivate(MerbSettings)
-    def self.deactivate
-    end
-    
-    #MerbSettings.setup_default_structure!
-
-    # Setup routes inside the host application
-    #
-    # @param scope<Merb::Router::Behaviour>
-    #  Routes will be added within this scope (namespace). In fact, any 
-    #  router behaviour is a valid namespace, so you can attach
-    #  routes at any level of your router setup.
-    #
-    # @note prefix your named routes with :merb_settings_
-    #   to avoid potential conflicts with global named routes.
-    def self.setup_router(scope)
-    end
-    
+  require 'yaml'
+  def run
+    RS.load_adapter!
   end
-  
-  # Add dependencies for other MerbSettings classes below. Example:
-  require "merb-settings/scoped"
-  
+
+  require 'rails-settings/scoped'  
 end
--- lib/rails-settings/railtie.rb
+++ lib/rails-settings/railtie.rb
@@ -2,8 +2,8 @@ module RailsSettings
   class Railtie << Rails::Railtie
     railtie_name :rails_settings
     
-    initializer "rails_settings.setup_settings" do |app|
-      
+    initializer "rails_settings.init_rails" do |app|
+      RailsSetttings.run
     end
   end
 end
